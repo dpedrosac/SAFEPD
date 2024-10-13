7985a5f7669a753bb552ba1656a30f8c0a28a7dd
FlorianKuschel <158284401+FlorianKuschel@users.noreply.github.com>
2024-10-13 13:41:08 +0200

    Create distribution.R

---
+++ distribution.R
@@ -0,0 +1,11 @@
+## Test auf Normalverteilung
+VarNVTest <- c("age","gender", "nationality", "martial_status", "years_since_diagnosis", "persons_houshold", "school_graduation", "professional_graduation", "employment_status", "UPDRS_I_Score", "UPDRS_II_Score", "lack_of_information", "uncertain_future",	"chaging_symptom_severity",	"gait_insecurity_fall",	"pain",	"gastrointestinal_symptoms",	"urinary_symptoms",	"mental_abilities",	"mental_symptoms",	"other_disease",	"nursing_care",	"side_effects_complications",	"access_healthcare",	"communication_with_me",	"communication_between_professionals",	"loneliness",	"everyday_problems",	"daily_routine",	"overload_among_people",	"pejorativ_looks_comments",	"family_role",	"conflicts_with_relatives",	"victim_to_crime",	"financial_worries",	"not_at_peace_with_myself",	"participation_in_road_traffic",	"overall_situation")
+
+shapiro_tests <- lapply(VarNVTest, function(variable) {
+  shapiro_test_result <- shapiro.test(SAFEPD[[variable]])
+  return(data.frame(Variable = variable, p_value = shapiro_test_result$p.value, 
+                    is_normal = shapiro_test_result$p.value > 0.05))})
+shapiro_results_table <- do.call(rbind, shapiro_tests)
+
+qqnorm(SAFEPD$age)
+qqline(SAFEPD$age)
