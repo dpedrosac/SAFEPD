eb8ac22683c96f4ca29bf536813f12bb322d2cbb
FlorianKuschel <158284401+FlorianKuschel@users.noreply.github.com>
2024-10-20 15:35:22 +0200

    Update 2_dichotomization.R

--- 2_dichotomization.R
+++ 2_dichotomization.R
@@ -23,12 +23,17 @@ SAFEPD[paste0(VarSAFEPDQA, "_Group")] <- lapply(SAFEPD[, VarSAFEPDQA], function(
 SAFEPD$UPDRS_Sum <- SAFEPD$UPDRS_I_Score + SAFEPD$UPDRS_II_Score
 
 # FIMA
-# SAFEPD$FIMA_1_Gesamt <- rowSums(SAFEPD[, c("FIMA_1_Hausarzt", "FIMA_1_Neurologe", "FIMA_1_Psychiater", "FIMA_1_Internist_FA", "FIMA_1_Gynaekologe", "FIMA_1_Urologe", "FIMA_1_Orthopaede", "FIMA_1_Notfall_KH", "FIMA_1_other")])
-# SAFEPD$FIMA_13_Mobilitaet <- ifelse(rowSums(SAFEPD[, c("FIMA_13_Gehstock", "FIMA_13_Anti_Freezing_Stock", "FIMA_13_Kamptokormie_Rucksack", "FIMA_13_Rollator", "FIMA_13_Rollstuhl", "FIMA_13_Treppenlift")]) > 0, 1, 0)
-# SAFEPD$FIMA_13_Hygiene <- ifelse(rowSums(SAFEPD[, c("FIMA_13_Badewannenlift", "FIMA_13_Badewanneneinstieghilfe", "FIMA_13_Badewannensitz", "FIMA_13_Toilettensitz", "FIMA_13_Toilettenstuhl", "FIMA_13_Urinflasche", "FIMA_13_Inkontinenzeinlagen")]) > 0, 1, 0)
-# SAFEPD$FIMA_13_Atmung <- ifelse(rowSums(SAFEPD[, c("FIMA_13_Sauerstoffgeraet", "FIMA_13_Schlafapnoe")]) > 0, 1, 0)
-# SAFEPD$FIMA_13_Tremor <- ifelse(rowSums(SAFEPD[, c("FIMA_13_Tremorbesteck", "FIMA_13_Greifzange")]) > 0, 1, 0)
-# SAFEPD$FIMA_13_Anzahl <- rowSums(SAFEPD[, c("FIMA_13_Gehstock", "FIMA_13_Anti_Freezing_Stock", "FIMA_13_Kamptokormie_Rucksack", "FIMA_13_Rollator", "FIMA_13_Rollstuhl", "FIMA_13_Treppenlift", "FIMA_13_Badewannenlift", "FIMA_13_Brille", "FIMA_13_Hoergeraet", "FIMA_13_Tremorbesteck", "FIMA_13_Greifzange", "FIMA_13_Sauerstoffgeraet", "FIMA_13_Schlafapnoe", "FIMA_13_Badewanneneinstieghilfe", "FIMA_13_Badewannensitz", "FIMA_13_Toilettensitz", "FIMA_13_Toilettenstuhl", "FIMA_13_Urinflasche", "FIMA_13_Inkontinenzeinlagen", "FIMA_13_Hausnotruf")])
+SAFEPD$FIMA_1_Gesamt <- rowSums(SAFEPD[, c("FIMA_1_Hausarzt", "FIMA_1_Neurologe", "FIMA_1_Psychiater", "FIMA_1_Internist_FA", "FIMA_1_Gynaekologe", "FIMA_1_Urologe", "FIMA_1_Orthopaede", "FIMA_1_Notfall_KH", "FIMA_1_other")])
+SAFEPD$FIMA_13_Mobilitaet <- ifelse(rowSums(SAFEPD[, c("FIMA_13_Gehstock", "FIMA_13_Anti_Freezing_Stock", "FIMA_13_Kamptokormie_Rucksack", "FIMA_13_Rollator", "FIMA_13_Rollstuhl", "FIMA_13_Treppenlift")]) > 0, 1, 0)
+SAFEPD$FIMA_13_Hygiene <- ifelse(rowSums(SAFEPD[, c("FIMA_13_Badewannenlift", "FIMA_13_Badewanneneinstieghilfe", "FIMA_13_Badewannensitz", "FIMA_13_Toilettensitz", "FIMA_13_Toilettenstuhl", "FIMA_13_Urinflasche", "FIMA_13_Inkontinenzeinlagen")]) > 0, 1, 0)
+SAFEPD$FIMA_13_Atmung <- ifelse(rowSums(SAFEPD[, c("FIMA_13_Sauerstoffgeraet", "FIMA_13_Schlafapnoe")]) > 0, 1, 0)
+SAFEPD$FIMA_13_Tremor <- ifelse(rowSums(SAFEPD[, c("FIMA_13_Tremorbesteck", "FIMA_13_Greifzange")]) > 0, 1, 0)
+SAFEPD$FIMA_13_Anzahl <- rowSums(SAFEPD[, c("FIMA_13_Gehstock", "FIMA_13_Anti_Freezing_Stock", "FIMA_13_Kamptokormie_Rucksack", "FIMA_13_Rollator", "FIMA_13_Rollstuhl", "FIMA_13_Treppenlift", "FIMA_13_Badewannenlift", "FIMA_13_Brille", "FIMA_13_Hoergeraet", "FIMA_13_Tremorbesteck", "FIMA_13_Greifzange", "FIMA_13_Sauerstoffgeraet", "FIMA_13_Schlafapnoe", "FIMA_13_Badewanneneinstieghilfe", "FIMA_13_Badewannensitz", "FIMA_13_Toilettensitz", "FIMA_13_Toilettenstuhl", "FIMA_13_Urinflasche", "FIMA_13_Inkontinenzeinlagen", "FIMA_13_Hausnotruf")])
+SAFEPD$FIMA_8_Group <- as.numeric(ifelse(SAFEPD$FIMA_8 == 0, "0", "1")) ## 0 = kein Pflegegrad
+SAFEPD$FIMA_9_Group <- as.numeric(ifelse(SAFEPD$FIMA_9 == 0, "0", "1")) ## 0 = keine Reha
+SAFEPD$FIMA_10_Group <- as.numeric(ifelse(SAFEPD$FIMA_10 == 0, "0", "1")) ## 0 = keine Tagesklinik
+SAFEPD$FIMA_11_Group <- as.numeric(ifelse(SAFEPD$FIMA_11 == 0, "0", "1")) ## 0 = kein Krankenhaus
+SAFEPD$FIMA_12_Group <- as.numeric(ifelse(SAFEPD$FIMA_12 == 0, "0", "1")) ## 0 = keine Psychiatrie
+SAFEPD$FIMA_14_Group <- as.numeric(ifelse(SAFEPD$FIMA_14 == 1, "0", "1")) ## 0 = zuhause wohnhaft 
 SAFEPD$FIMA_16_Group <- as.numeric(ifelse(SAFEPD$FIMA_16 == 1 , "0", "1")) ## 0 = gesetzlich versichert
-
 
