d74acd8f9492a4d048f2ceeb4f8e457851db8e42
David Pedrosa <david.pedrosa@staff.uni-marburg.de>
2024-11-14 23:17:06 +0100

    Start with some suggestions for the code

--- 1_perform_analysis.R
+++ 1_perform_analysis.R
@@ -1,10 +1,30 @@
-## Pakete laden
-source("loadpackages.R")
+# Run analyses for a meta-analysis on place of death of people with Parkinson's Disease (PwPD): 
+# Code developed by Florian Kuschel and David Pedrosa
 
-## Datei laden
-SAFEPD <- read_excel("~/SAFEPD/SAFEPD_imputeddata.xlsx", 
-                     range = "A1:DF209",
-                     col_types = c(rep("numeric", 110)), n_max = 210)
+# Version 2.1 # 2024-14-11, # Smaller adjustments to make the code readble for different collaborators
+
+## Load required packages from external script
+source("load_packages.R")
+
+## Load data according to the username
+username <- Sys.getenv("USERNAME")
+
+if (username == "dpedrosac") {
+  wdir <- "/media/storage/SAFEPD/"
+  datadir <- file.path(wdir, "data")
+} else {
+  wdir <- "~/SAFEPD/"
+  datadir <- file.path(wdir, "data") # you need to move the file to this folder. I have added folder to .gitignore
+}
+
+# Load the SAFEPD dataset with specific range and column types
+df_safepd <- read_excel(
+  file.path(datadir, "SAFEPD_imputeddata.xlsx"),
+  range = "A1:DF209",
+  col_types = rep("numeric", 110),  # Set 110 columns as numeric
+  n_max = 210                       # Load only the first 210 rows
+)
+
 
 ## Dichotomisierung / Imputation
 source("SAFEPD_dich.R")
