ded926c14b3330dc090de113123beba40f57485e
FlorianKuschel <158284401+FlorianKuschel@users.noreply.github.com>
2024-11-24 20:11:23 +0100

    Update SAFEPD_hypothesis.R

--- SAFEPD_hypothesis.R
+++ SAFEPD_hypothesis.R
@@ -5,24 +5,34 @@ doctor_columns <- c("FIMA_1_Hausarzt", "FIMA_1_Neurologe", "FIMA_1_Psychiater", 
 df_safepd$visited_two_or_more <- ifelse(rowSums(df_safepd[, doctor_columns] > 0, na.rm = TRUE) >= 2, 1, 0)
 # Korrelation
 cor.test(df_safepd$visited_two_or_more, df_safepd$nursing_care_Group, method = "pearson", use = "complete.obs")
+chisq.test(df_safepd$visited_two_or_more, df_safepd$nursing_care_Group)
 cor.test(df_safepd$visited_two_or_more, df_safepd$communication_with_me_Group, method = "pearson", use = "complete.obs")
+chisq.test(df_safepd$visited_two_or_more, df_safepd$communication_with_me_Group)
 cor.test(df_safepd$visited_two_or_more, df_safepd$communication_between_professionals_Group, method = "pearson", use = "complete.obs")
+chisq.test(df_safepd$visited_two_or_more, df_safepd$communication_between_professionals_Group)
 
 # Hypothese: positive Assoziation von Sicherheit & guter Pflege 
 cor.test(df_safepd$overall_situation_Group, df_safepd$nursing_care_Group, method = "pearson", use = "complete.obs")
+chisq.test(df_safepd$overall_situation_Group, df_safepd$nursing_care_Group)
 
 # Hypothese: positive Assoziation von Sicherheit & Hilfe von Familienangehörigen (Milberg et al.)
 cor.test(df_safepd$overall_situation_Group, df_safepd$FIMA_5, method = "pearson", use = "complete.obs")
+chisq.test(df_safepd$overall_situation_Group, df_safepd$FIMA_5)
 
 # Hypothese: negative Assoziation von Sicherheit & finanziellen Problemen
 cor.test(df_safepd$overall_situation_Group, df_safepd$financial_worries_Group, method = "pearson", use = "complete.obs")
+chisq.test(df_safepd$overall_situation_Group, df_safepd$financial_worries_Group)
 
 # Hypothese: schlechteres psychosoziales Funktionsniveau bei Frauen (Perepezko et al.)
 cor.test(df_safepd$loneliness_Group, df_safepd$gender_Group, method = "pearson", use = "complete.obs")
+chisq.test(df_safepd$loneliness_Group, df_safepd$gender_Group)
+chisq.test(df_safepd$loneliness_Group, df_safepd$persons_houshold_Group)
 H1 <- glm(df_safepd$loneliness_Group ~ df_safepd$gender_Group * df_safepd$persons_houshold_Group, family = "binomial")
 summary(H1)
 
 cor.test(df_safepd$not_at_peace_with_myself_Group, df_safepd$gender_Group, method = "pearson", use = "complete.obs")
+chisq.test(df_safepd$not_at_peace_with_myself_Group, df_safepd$gender_Group)
+chisq.test(df_safepd$not_at_peace_with_myself_Group, df_safepd$persons_houshold_Group)
 H2 <- glm(df_safepd$not_at_peace_with_myself_Group ~ df_safepd$gender_Group * df_safepd$persons_houshold_Group, family = "binomial")
 summary(H2)
 
@@ -31,24 +41,29 @@ cor.test(df_safepd$persons_houshold_Group, df_safepd$loneliness_Group, method = 
 
 # Hypothese: schlechtere soziale Unterstützung bei Frauen (Perepezko et al.)
 cor.test(df_safepd$FIMA_5, df_safepd$gender_Group, method = "pearson", use = "complete.obs")
+chisq.test(df_safepd$FIMA_5, df_safepd$gender_Group)
 H3 <- glm(df_safepd$FIMA_5 ~ df_safepd$gender_Group, family = "binomial")
 summary(H3)
 
 # Hypothese: bessere Paarbeziehung (= family role???) bei älteren Paaren (Perepezko et al.)
 cor.test(df_safepd$family_role_Group, df_safepd$age, method = "pearson", use = "complete.obs")
+wilcox.test(df_safepd$family_role_Group, df_safepd$age)
 H4 <- glm(df_safepd$family_role_Group ~ df_safepd$age, family = "binomial")
 summary(H4)
 
 # Hypothese: Unsicherheit vor der Zukunft mit geringerer Beziehungsqualität (family role???) verbunden (ebd.)
 cor.test(df_safepd$family_role_Group, df_safepd$uncertain_future_Group, method = "pearson", use = "complete.obs")
+chisq.test(df_safepd$family_role_Group, df_safepd$uncertain_future_Group)
 H5 <- glm(df_safepd$family_role_Group ~ df_safepd$uncertain_future_Group, family = "binomial")
 summary(H5)
 
 # Hypothese: höheres Alter, längere Krankheitsdauer und weibliches Geschlecht negativ mit Berufsleben assoziiert (ebd.)
-# H6 <- glm(df_safepd$employment_status_Group)
+H6 <- glm(df_safepd$employment_status_Group  ~ df_safepd$age + df_safepd$years_since_diagnosis + df_safepd$gender_Group)
+summary(H6)
 
 # Hypothese: schlechteres allgemeines soziales Funktionsniveau bei kognitiver Beeinträchtigung (ebd.)
-# cor.test(df_safepd$mental_abilities_Group)
+cor.test(df_safepd$mental_abilities_Group, df_safepd$family_role_Group)
+cor.test(df_safepd$mental_abilities_Group, df_safepd$overload_among_people_Group)
 
 # Hypothese: jüngere Patienten fühlen sich bei stationärem Aufenthalt sicherer als ältere Patienten (Robinson)
 H7 <- glm(df_safepd$overall_situation_Group ~ df_safepd$age * df_safepd$FIMA_11_Group, family = gaussian())
