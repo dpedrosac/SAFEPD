89d9284eb54725957fab8cfcc1f3df359de9d0c2
FlorianKuschel <158284401+FlorianKuschel@users.noreply.github.com>
2024-11-24 10:42:46 +0100

    Update and rename loadpackages.R to load_packages.R

---
+++ load_packages.R
@@ -0,0 +1,38 @@
+packages <- c(
+  "tidyverse", 
+  "dplyr", 
+  "descr", 
+  "sjmisc", 
+  "sjlabelled", 
+  "sjPlot", 
+  "sjstats", 
+  "easystats", 
+  "ggpubr", 
+  "psych",
+  "tableone", 
+  "mice", 
+  "VIM",
+  "car",
+  "MASS",
+  "brant",
+  "gofcat",
+  "pscl",
+  "ordinal",
+  "openxlsx",
+  "gplots",
+  "openxlsx",
+  "ggplot2"
+)
+
+## Now load or install & load all if necessary
+package.check <- lapply(
+  packages,
+  FUN = function(x) {
+    if (!require(x, character.only = TRUE)) {
+      install.packages(x, dependencies = TRUE)
+      library(x, character.only = TRUE)
+    }
+  }
+)
+
+
