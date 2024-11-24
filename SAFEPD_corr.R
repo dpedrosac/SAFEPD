3e8c940e98a879f1629f05855bfed6ab2d9342ab
FlorianKuschel <158284401+FlorianKuschel@users.noreply.github.com>
2024-11-24 13:38:40 +0100

    Update SAFEPD_corr.R

--- SAFEPD_corr.R
+++ SAFEPD_corr.R
@@ -1,9 +1,10 @@
+
 # Korrelationsanalyse: SozDem ~ SozDem
-correlation_Soz <- cor(na.omit(SAFEPD[, 2:11])) 
+correlation_Soz <- cor(na.omit(df_safepd[, 2:11])) 
 corrplot(correlation_Soz , method = "color", addrect = 2)
 
-# Korrelationsanalyse: SAFEPDQA ~ overall_situation
-correlation_matrix <- cor(na.omit(SAFEPD[, 12:38]))
+# Korrelationsanalyse: df_safepdQA ~ overall_situation
+correlation_matrix <- cor(na.omit(df_safepd[, 12:38]))
 corrplot(correlation_matrix, method = "color", addrect = 2)
 
 highest_correlation_matrix <- correlation_matrix
@@ -24,7 +25,7 @@ corrplot(low_correlation_matrix, method = "color", addrect = 2)
 
 # Korrelationsanalyse: FIMA-Daten ~ overall_situation
 columns_FIMA <- c(62:77, 148:152, 38)
-correlation_FIMA <- cor(na.omit(SAFEPD[, columns_FIMA]))
+correlation_FIMA <- cor(na.omit(df_safepd[, columns_FIMA]))
 corrplot(correlation_FIMA, method = "color", addrect = 2)
 
 # Gruppenvergleich: FIMA-Daten ~ overall_situation
@@ -39,28 +40,39 @@ corrplot(correlation_FIMA, method = "color", addrect = 2)
 #  return(result)}
 
 # FIMA_analysen <- list(
-#  analyse_FIMA(overall_situation_Group ~ FIMA_1_Hausarzt, SAFEPD, "FIMA_1_Hausarzt", "overall_situation_Group"),
-#  analyse_FIMA(overall_situation_Group ~ FIMA_1_Neurologe, SAFEPD, "FIMA_1_Neurologe", "overall_situation_Group"),
-#  analyse_FIMA(overall_situation_Group ~ FIMA_1_Psychiater, SAFEPD, "FIMA_1_Psychiater", "overall_situation_Group"),
-#  analyse_FIMA(overall_situation_Group ~ FIMA_1_Internist_FA, SAFEPD, "FIMA_1_Internist_FA", "overall_situation_Group"),
-#  analyse_FIMA(overall_situation_Group ~ FIMA_1_Urologe, SAFEPD, "FIMA_1_Urologe", "overall_situation_Group"),
-#  analyse_FIMA(overall_situation_Group ~ FIMA_1_Gynaekologe, SAFEPD, "FIMA_1_Gynaekologe", "overall_situation_Group"),
-#  analyse_FIMA(overall_situation_Group ~ FIMA_1_Orthopaede, SAFEPD, "FIMA_1_Orthopaede", "overall_situation_Group"),
-#  analyse_FIMA(overall_situation_Group ~ FIMA_1_Notfall_KH, SAFEPD, "FIMA_1_Notfall_KH", "overall_situation_Group"),
-#  analyse_FIMA(overall_situation_Group ~ FIMA_1_other, SAFEPD, "FIMA_1_other", "overall_situation_Group"),
-#  analyse_FIMA(overall_situation_Group ~ FIMA_1_Gesamt, SAFEPD, "FIMA_1_Gesamt", "overall_situation_Group"),
-#  analyse_FIMA(overall_situation_Group ~ FIMA_2_Krankengymnastik, SAFEPD, "FIMA_2_Krankengymnastik", "overall_situation_Group"),
-#  analyse_FIMA(overall_situation_Group ~ FIMA_2_Ergotherapie, SAFEPD, "FIMA_2_Ergotherapie", "overall_situation_Group"),
-#  analyse_FIMA(overall_situation_Group ~ FIMA_2_Sprachtherapie, SAFEPD, "FIMA_2_Sprachtherapie", "overall_situation_Group"),
-#  analyse_FIMA(overall_situation_Group ~ FIMA_2_Heilpraktiker, SAFEPD, "FIMA_2_Heilpraktiker", "overall_situation_Group"),
-#  analyse_FIMA(overall_situation_Group ~ FIMA_2_Osteopath, SAFEPD, "FIMA_2_Osteopath", "overall_situation_Group"),
-#  analyse_FIMA(overall_situation_Group ~ FIMA_2_Chiropraktiker, SAFEPD, "FIMA_2_Chiropraktiker", "overall_situation_Group"),
-#  analyse_FIMA(overall_situation_Group ~ FIMA_2_Psychotherapeut, SAFEPD, "FIMA_2_Psychotherapeut", "overall_situation_Group"),
-#  analyse_FIMA(overall_situation_Group ~ FIMA_16_Group, SAFEPD, "FIMA_16_Group", "overall_situation_Group"),
-#  analyse_FIMA(overall_situation_Group ~ FIMA_13_Mobilitaet, SAFEPD, "FIMA_13_Mobilitaet", "overall_situation_Group"),
-#  analyse_FIMA(overall_situation_Group ~ FIMA_13_Hygiene, SAFEPD, "FIMA_13_Hygiene", "overall_situation_Group"),
-#  analyse_FIMA(overall_situation_Group ~ FIMA_13_Tremor, SAFEPD, "FIMA_13_Tremor", "overall_situation_Group"),
-#  analyse_FIMA(overall_situation_Group ~ FIMA_13_Atmung, SAFEPD, "FIMA_13_Atmung", "overall_situation_Group"),
-#  analyse_FIMA(overall_situation_Group ~ FIMA_13_Hausnotruf, SAFEPD, "FIMA_13_Hausnotruf", "overall_situation_Group"),
-#  analyse_FIMA(overall_situation_Group ~ FIMA_13_Anzahl, SAFEPD, "FIMA_13_Anzahl", "overall_situation_Group"))
+#  analyse_FIMA(overall_situation_Group ~ FIMA_1_Hausarzt, df_safepd, "FIMA_1_Hausarzt", "overall_situation_Group"),
+#  analyse_FIMA(overall_situation_Group ~ FIMA_1_Neurologe, df_safepd, "FIMA_1_Neurologe", "overall_situation_Group"),
+#  analyse_FIMA(overall_situation_Group ~ FIMA_1_Psychiater, df_safepd, "FIMA_1_Psychiater", "overall_situation_Group"),
+#  analyse_FIMA(overall_situation_Group ~ FIMA_1_Internist_FA, df_safepd, "FIMA_1_Internist_FA", "overall_situation_Group"),
+#  analyse_FIMA(overall_situation_Group ~ FIMA_1_Urologe, df_safepd, "FIMA_1_Urologe", "overall_situation_Group"),
+#  analyse_FIMA(overall_situation_Group ~ FIMA_1_Gynaekologe, df_safepd, "FIMA_1_Gynaekologe", "overall_situation_Group"),
+#  analyse_FIMA(overall_situation_Group ~ FIMA_1_Orthopaede, df_safepd, "FIMA_1_Orthopaede", "overall_situation_Group"),
+#  analyse_FIMA(overall_situation_Group ~ FIMA_1_Notfall_KH, df_safepd, "FIMA_1_Notfall_KH", "overall_situation_Group"),
+#  analyse_FIMA(overall_situation_Group ~ FIMA_1_other, df_safepd, "FIMA_1_other", "overall_situation_Group"),
+#  analyse_FIMA(overall_situation_Group ~ FIMA_1_Gesamt, df_safepd, "FIMA_1_Gesamt", "overall_situation_Group"),
+#  analyse_FIMA(overall_situation_Group ~ FIMA_2_Krankengymnastik, df_safepd, "FIMA_2_Krankengymnastik", "overall_situation_Group"),
+#  analyse_FIMA(overall_situation_Group ~ FIMA_2_Ergotherapie, df_safepd, "FIMA_2_Ergotherapie", "overall_situation_Group"),
+#  analyse_FIMA(overall_situation_Group ~ FIMA_2_Sprachtherapie, df_safepd, "FIMA_2_Sprachtherapie", "overall_situation_Group"),
+#  analyse_FIMA(overall_situation_Group ~ FIMA_2_Heilpraktiker, df_safepd, "FIMA_2_Heilpraktiker", "overall_situation_Group"),
+#  analyse_FIMA(overall_situation_Group ~ FIMA_2_Osteopath, df_safepd, "FIMA_2_Osteopath", "overall_situation_Group"),
+#  analyse_FIMA(overall_situation_Group ~ FIMA_2_Chiropraktiker, df_safepd, "FIMA_2_Chiropraktiker", "overall_situation_Group"),
+#  analyse_FIMA(overall_situation_Group ~ FIMA_2_Psychotherapeut, df_safepd, "FIMA_2_Psychotherapeut", "overall_situation_Group"),
+#  analyse_FIMA(overall_situation_Group ~ FIMA_16_Group, df_safepd, "FIMA_16_Group", "overall_situation_Group"),
+#  analyse_FIMA(overall_situation_Group ~ FIMA_13_Mobilitaet, df_safepd, "FIMA_13_Mobilitaet", "overall_situation_Group"),
+#  analyse_FIMA(overall_situation_Group ~ FIMA_13_Hygiene, df_safepd, "FIMA_13_Hygiene", "overall_situation_Group"),
+#  analyse_FIMA(overall_situation_Group ~ FIMA_13_Tremor, df_safepd, "FIMA_13_Tremor", "overall_situation_Group"),
+#  analyse_FIMA(overall_situation_Group ~ FIMA_13_Atmung, df_safepd, "FIMA_13_Atmung", "overall_situation_Group"),
+#  analyse_FIMA(overall_situation_Group ~ FIMA_13_Hausnotruf, df_safepd, "FIMA_13_Hausnotruf", "overall_situation_Group"),
+#  analyse_FIMA(overall_situation_Group ~ FIMA_13_Anzahl, df_safepd, "FIMA_13_Anzahl", "overall_situation_Group"))
 # ergebnisse_FIMA <- do.call(rbind, FIMA_analysen)
+
+cor(df_safepd$UPDRS_II_Score, df_safepd$UPDRS_I_Score, method = "spearman", use = "complete.obs")
+
+ggplot(correlation_results, aes(x = reorder(Item, Correlation), y = Correlation)) +
+  geom_bar(stat = "identity", fill = "blue") +
+  coord_flip() +
+  labs(title = "Korrelation der Items zur Overall Situation", x = "Items", y = "Korrelation") +
+  theme_minimal()
+
+
+
